f9009ebe874480a01a38d77d833eb6f88d7a9a66
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-10-30 21:59:11 -0300

    Title atualizado

--- app/Views/default.php
+++ app/Views/default.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <title>Document</title>
+    <title>Massas para pizza</title>
 </head>
 <body>
     <?= $this->renderSection('conteudo') ?>
