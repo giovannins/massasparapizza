df004821d8639242e713206cdc3531ea7fdc7b80
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-10-30 21:56:53 -0300

    Admin Login

---
+++ app/Views/admin/index.php
@@ -0,0 +1,17 @@
+<?= $this->extend('default') ?>
+
+<?= $this->section('conteudo') ?>
+<div class="card position-absolute top-50 start-50 translate-middle" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title text-center">Administrador</h5>
+        <form action="api/admin/login" method="post">
+            <?= csrf_field() ?>
+            <div class="my-3">
+                <label for="adminPass" class="form-label">Senha:</label>
+                <input type="password" class="form-control" id="adminPass" name="adminPass">
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+    </div>
+</div>
+<?= $this->endSection() ?>
