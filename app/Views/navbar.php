7e981a6fee2d41759d75e507a936f9e91e5d80bf
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-11-01 11:37:25 -0300

    Atualizações no html base

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-success navbar-dark">
     <div class="container-fluid">
         <img src="/imgs/Logo.svg" alt="MassaParaPizza" width="48" height="48" class="d-inline-block align-text-top">
-        <a class="navbar-brand" href="#">Massa Para Pizza</a>
+        <a class="navbar-brand" href="/">Massa Para Pizza</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
