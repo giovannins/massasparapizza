0bd5e0aba7d98db9b4dda6a42152833fb2ff3dde
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-11-01 17:36:43 -0300

    Session no controller de admin

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\ProdutoModel;
+use CodeIgniter\Config\Services as ConfigServices;
+use CodeIgniter\Session\Session;
+use Config\Services;
 
 class AdminController extends BaseController
 {
@@ -14,18 +17,43 @@ class AdminController extends BaseController
 
     public function painel()
     {
+        $session = session();
+        if (!$session->get('is_login')) {
+            return redirect()->to('/');
+        }
         $produtosModel = new ProdutoModel();
         return view('admin/painel.php', ['produtos' => $produtosModel->findAll()]);
     }
 
     public function editar($id)
     {
+        $session = session();
+        if (!$session->get('is_login')) {
+            return redirect()->to('/');
+        }
         $produtosModel = new ProdutoModel();
-        return view('admin/painel.php', ['produtos' => $produtosModel->find($id)]);
+        return view('admin/editar.php', ['produtos' => $produtosModel->find($id)]);
     }
 
     public function criar()
     {
+        $session = session();
+        if (!$session->get('is_login')) {
+            return redirect()->to('/');
+        }
         return view('admin/criar.php');
+    }
+
+    public function login()
+    {
+        if (!$this->validate(['adminPass' => 'required'])) {
+            return redirect()->to('/');
+        }
+        if ($this->request->getPost('adminPass') != env('admin.pass')) {
+            return redirect()->to('/');
+        }
+        $session = session();
+        $session->set('is_login', true);
+        return redirect()->to('/admin/painel');
     }
 }
