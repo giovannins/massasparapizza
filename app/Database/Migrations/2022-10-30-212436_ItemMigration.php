fd4f34520fed5dc21618d7f097a48d6800e32745
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-11-01 10:02:08 -0300

    Migrations

--- app/Database/Migrations/2022-10-30-212436_ItemMigration.php
+++ app/Database/Migrations/2022-10-30-212436_ItemMigration.php
@@ -8,7 +8,28 @@ class ItemMigration extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'int',
+                'constraint' => 8,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'tamanho' => [
+                'type' => 'int',
+                'constraint' => 8,
+            ],
+            'valor' => [
+                'type' => 'float',
+                'constraint' => 8,
+            ],
+            'quantidade' => [
+                'type' => 'int',
+                'constraint' => 8,
+            ],
+        ]);
+        $this->forge->addKey('id', true, true);
+        $this->forge->createTable('itens');
     }
 
     public function down()
--- app/Database/Migrations/2022-10-30-212454_CategoriaMigration.php
+++ app/Database/Migrations/2022-10-30-212454_CategoriaMigration.php
@@ -8,7 +8,20 @@ class CategoriaMigration extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'int',
+                'constraint' => 8,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'tamanho' => array(
+                'type' => 'varchar',
+                'constraint' => 100,
+            ),
+        ]);
+        $this->forge->addKey('id', true, true);
+        $this->forge->createTable('categorias');
     }
 
     public function down()
